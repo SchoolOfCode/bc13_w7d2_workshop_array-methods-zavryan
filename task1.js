a92f80c14743646794b9a39b873d51cf7426d2a5
ryandonaghue <ryandonaghue@outlook.com>
2022-11-08 12:30:00 +0000

    Attempted task 1

--- task1.js
+++ task1.js
@@ -1,3 +1,9 @@
 const cats = ["tony", "daisy", "socks", "rockie" ];
 
-const capitalisedCats = // your code here
+const capitalisedCats = cats.map((uppercase)=>{
+return uppercase.toUppercase
+})
+console.log(capitalisedCats);
+
+// Write a function which takes in a string turns the letters to uppercase
+// use .map() to generate a new array called capitalisedCats.
