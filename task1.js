cddf61f6f9c4d722843c7d26a18ae34ef952e409
mvhmz81 <mvhmz@yahoo.co.uk>
2022-11-08 13:44:08 +0000

    task 2 complete

--- task1.js
+++ task1.js
@@ -11,3 +11,5 @@ console.log(capitalisedCats);
 
 // Write a function which takes in a string turns the letters to uppercase
 // use .map() to generate a new array called capitalisedCats.
+
+//practice
--- task2.js
+++ task2.js
@@ -1,11 +1,16 @@
 let animals = [
-    "baboon",
-    "kangaroo",
-    "rhino",
-    "frog",
-    "beaver",
-    "horse",
-    "basalisk",
-  ];
-  
-let bListAnimals = // Your 'filter' code here.
+  "baboon",
+  "kangaroo",
+  "rhino",
+  "frog",
+  "beaver",
+  "horse",
+  "basalisk",
+];
+// need to look into the array and filter all animals
+//that begin with b
+let bListAnimals = animals.filter((blist) => {
+  //return blist.startsWith("b");
+  return blist[0] === "b";
+}); // Your 'filter' code here.
+console.log(bListAnimals);
